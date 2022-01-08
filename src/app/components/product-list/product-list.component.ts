fd47bc41b185c14883e60f0717aeb4fa33449377
Paul Santiago <paulsantiago282@gmail.com>
2022-01-08 15:48:34 -0500

    Update grid, update product service to search by category id in url, add ! to triage if object is null

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -1,26 +1,46 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { Product } from 'src/app/common/product';
 import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list-grid.component.html',
-  //templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent implements OnInit {
 
   products: Product[];
+  currentCategoryId: number;
 
-  // Inject the product service for this component to use it
-  constructor(private productService: ProductService) { }
+  // Inject the pronewPropertye for this component to use it
+  constructor(private productService: ProductService,
+              private route: ActivatedRoute) { }
 
   ngOnInit() {
-    this.listProducts();
+    this.route.paramMap.subscribe(() => {
+      this.listProducts();
+    })
   }
 
   listProducts(){
-    this.productService.getProductList().subscribe(
+
+    // check if id" parameter is available
+    //route -> use the activated route, snapshot -> state of route at the given moment of time
+    //paramMap -> map of all the route parameters .has('id') read the id parameter
+    const hasCategoryId: boolean = this.route.snapshot.paramMap.has('id');
+
+    if(hasCategoryId){
+      // get the "id" param string. convert string to a number using the "+ symobl"
+      this.currentCategoryId = +this.route.snapshot.paramMap.get('id')!;
+    }
+    else{
+      // not category id available.. default  category id1
+      this.currentCategoryId = 1;
+    }
+
+    // no get the products for the given category id
+    this.productService.getProductList(this.currentCategoryId).subscribe(
       data => {
         this.products = data;
       }
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -9,14 +9,18 @@ import {map} from 'rxjs/operators';
 })
 export class ProductService {
 
-  private baseUrl='http://localhost:8080/api/products?size=100';
+  private baseUrl='http://localhost:8080/api/products';
 
   constructor(private httpClient: HttpClient) { }
 
 // returns an observable of type Product array and maps JSON data from Spring Data Rest to product Array
 //utilizes the GetResponse interface which "unwraps" the JSON data that you get from the product Array
-  getProductList(): Observable<Product[]>{
-    return this.httpClient.get<GetResponse>(this.baseUrl).pipe(
+  getProductList(theCategoryId: number): Observable<Product[]>{
+
+    // build url based on category id
+    const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}`;
+
+    return this.httpClient.get<GetResponse>(searchUrl).pipe(
       // _embedded is the top level object with products as the array
       map(response => response._embedded.products)
     );
