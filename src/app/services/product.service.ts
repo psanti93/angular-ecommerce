280c7030637f5b39e84b4486f9d634110ef13673
Paul Santiago <paulsantiago282@gmail.com>
2022-01-11 17:18:55 -0500

    Create get method to retrieve array of product categories from the spring be

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Product } from '../common/product';
 import {map} from 'rxjs/operators';
+import { ProductCategory } from '../common/product-category';
 
 @Injectable({
   providedIn: 'root'
@@ -11,6 +12,7 @@ export class ProductService {
 
 
   private baseUrl='http://localhost:8080/api/products';
+  private categoryUrl = "http://localhost:8080/api/products/product-category";
 
   constructor(private httpClient: HttpClient) { }
 
@@ -21,21 +23,29 @@ export class ProductService {
     // build url based on category id
     const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}`;
 
-    return this.httpClient.get<GetResponse>(searchUrl).pipe(
+    return this.httpClient.get<GetResponseProducts>(searchUrl).pipe(
       // _embedded is the top level object with products as the array
       map(response => response._embedded.products)
     );
   }
+
+  getProductCategories(): Observable<ProductCategory[]> {
+    return this.httpClient.get<GetResponseProductCategory>(this.categoryUrl).pipe(
+      map(response => response._embedded.productCategory)
+    );
+  }
 }
 
-  getProductCategories() {
-    throw new Error('Method not implemented.');
-  }
-
-
-// unwraps the JSOn from Spring Data Rest _embedded entry
-interface GetResponse{
+// unwraps the JSOn from Spring Data Rest _embedded entry for products
+interface GetResponseProducts{
   _embedded: {
     products: Product [];
   }
  }
+
+ // unwraps the JSOn from Spring Data Rest _embedded entry for product categories
+ interface GetResponseProductCategory{
+   _embedded: {
+     productCategory: ProductCategory [];
+   }
+  }
