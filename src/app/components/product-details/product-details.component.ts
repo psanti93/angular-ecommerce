de422c9ea8450c99913ec2aed4191183316ba997
Paul Santiago <paulsantiago282@gmail.com>
2022-01-13 18:51:55 -0500

    Add info to resolve race condition

--- src/app/components/product-details/product-details.component.ts
+++ src/app/components/product-details/product-details.component.ts
@@ -10,7 +10,7 @@ import { ProductService } from 'src/app/services/product.service';
 })
 export class ProductDetailsComponent implements OnInit {
 
-  product: Product;
+  product: Product = new Product(); //one solution is to instantiate the product
 
   constructor(private productService: ProductService, private route: ActivatedRoute) { }
 
