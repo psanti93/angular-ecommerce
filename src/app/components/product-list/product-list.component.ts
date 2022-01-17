00b47e1a29267539254733c3317905c8cb534fb1
Paul Santiago <paulsantiago282@gmail.com>
2022-01-17 12:13:53 -0500

    Inject cart serviice in product-list component and use addToCart method to add the product

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { CartItem } from 'src/app/common/cart-item';
 import { Product } from 'src/app/common/product';
+import { CartService } from 'src/app/services/cart.service';
 import { GetResponseProducts, ProductService } from 'src/app/services/product.service';
 
 @Component({
@@ -24,6 +26,7 @@ export class ProductListComponent implements OnInit {
 
   // Inject the pronewPropertye for this component to use it
   constructor(private productService: ProductService,
+              private cartService: CartService,
               private route: ActivatedRoute) { }
 
   ngOnInit() {
@@ -113,6 +116,8 @@ export class ProductListComponent implements OnInit {
   addToCart(theProduct: Product){
     console.log(`Adding to cart: ${theProduct.name}, ${theProduct.unitPrice}`)
     //TODO add logic to add to cart
+    const theCartItem = new CartItem(theProduct);
+    this.cartService.addToCart(theCartItem);
   }
 
 }
